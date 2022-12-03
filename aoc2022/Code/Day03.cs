c0e9f68699928cf337f81a48bf66bfa532df9bc4
Szczepan Ćwikliński <sec@thinkcode.pl>
2022-12-03 13:11:46 +0100

    Day 03

---
+++ aoc2022/Code/Day03.cs
@@ -0,0 +1,41 @@
+﻿namespace aoc2022.Code;
+
+internal class Day03 : BaseDay
+{
+    static int GetPriority(char item) => char.IsUpper(item) ? item - 38 : item - 96;
+
+    static List<char> GetCommon(string rucksack)
+    {
+        var left = rucksack.Substring(0, rucksack.Length / 2);
+        var right = rucksack.Substring(rucksack.Length / 2);
+
+        return left.Where(right.Contains).Distinct().ToList();
+    }
+
+    protected override object Part1() => ReadAllLines().Sum(x => GetCommon(x).Sum(GetPriority));
+
+    protected override object Part2()
+    {
+        var sum = 0;
+
+        foreach (var rucksacks in ReadAllLines().Chunk(3))
+        {
+            var a = rucksacks[0];
+            var b = rucksacks[1];
+            var c = rucksacks[2];
+
+            foreach (var item in a)
+            {
+                if (!b.Contains(item) || !c.Contains(item))
+                {
+                    continue;
+                }
+
+                sum += GetPriority(item);
+                break;
+            }
+        }
+
+        return sum;
+    }
+}
