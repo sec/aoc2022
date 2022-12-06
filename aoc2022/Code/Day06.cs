6bb555751cc22f98864e689978e2aac5abf7d1e0
Szczepan Ćwikliński <sec@thinkcode.pl>
2022-12-06 06:16:37 +0100

    Day 06

---
+++ aoc2022/Code/Day06.cs
@@ -0,0 +1,30 @@
+﻿namespace aoc2022.Code;
+
+internal class Day06 : BaseDay
+{
+    static int Decode(string signal, int magicNumber)
+    {
+        var last = new List<char>();
+
+        for (var i = 0; i < signal.Length; i++)
+        {
+            last.Add(signal[i]);
+
+            if (last.Skip(1).Distinct().Count() == magicNumber)
+            {
+                return i + 1;
+            }
+
+            if (last.Count > magicNumber)
+            {
+                last.RemoveAt(0);
+            }
+        }
+
+        return -1;
+    }
+
+    protected override object Part1() => Decode(ReadAllText(), 4);
+
+    protected override object Part2() => Decode(ReadAllText(), 14);
+}
