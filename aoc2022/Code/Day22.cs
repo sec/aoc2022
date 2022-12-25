84f54c9c2f2a26de7b98e922018843e1f7c3ec9c
Szczepan Ćwikliński <sec@thinkcode.pl>
2022-12-25 19:38:40 +0100

    Day 22 (w.i.p.)

--- aoc2022/Code/Day22.cs
+++ aoc2022/Code/Day22.cs
@@ -45,7 +45,7 @@ internal class Day22 : BaseDay
             }
         }
 
-        public Map Walk()
+        public Map Walk(bool use3d)
         {
             var sb = new StringBuilder();
             foreach (var c in _path)
@@ -56,12 +56,12 @@ internal class Day22 : BaseDay
                 }
                 else
                 {
-                    Move(int.Parse(sb.ToString()));
+                    Move(int.Parse(sb.ToString()), use3d);
                     Turn(c);
                     sb.Clear();
                 }
             }
-            Move(int.Parse(sb.ToString()));
+            Move(int.Parse(sb.ToString()), use3d);
 
             return this;
         }
@@ -116,7 +116,7 @@ internal class Day22 : BaseDay
             throw new InvalidDataException();
         }
 
-        private void Move(int v)
+        private void Move(int v, bool use3d)
         {
             var (addX, addY) = _moves[(int) _face];
             while (v-- > 0)
@@ -133,19 +133,35 @@ internal class Day22 : BaseDay
                 {
                     var x = -1;
                     var y = -1;
-                    switch (_face)
+                    if (!use3d)
                     {
-                        case Face.Left:
-                        case Face.Right:
-                            y = _cy;
-                            x = WrapX(y, _face == Face.Left ? -1 : 1);
-                            break;
+                        switch (_face)
+                        {
+                            case Face.Left:
+                            case Face.Right:
+                                y = _cy;
+                                x = WrapX(y, _face == Face.Left ? -1 : 1);
+                                break;
 
-                        case Face.Down:
-                        case Face.Up:
-                            x = _cx;
-                            y = WrapY(x, _face == Face.Down ? 1 : -1);
-                            break;
+                            case Face.Down:
+                            case Face.Up:
+                                x = _cx;
+                                y = WrapY(x, _face == Face.Down ? 1 : -1);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        // 3d magic here
+                        x = _cx;
+                        y = _cy;
+
+                        var mapping = GetMapping();
+
+                        var cubeX = _cx / 50;
+                        var cubeY = _cy / 50;
+
+                        var map = mapping[(cubeX, cubeY, _face)];
                     }
 
                     if (Empty(x, y))
@@ -158,12 +174,45 @@ internal class Day22 : BaseDay
         }
 
         public int Password => 1000 * (_cy + 1) + 4 * (_cx + 1) + (int) _face;
+
+        Dictionary<(int CubeX, int CubeY, Face Dir), (int CubeX, int CubeY, Face Dir)> GetMapping()
+        {
+            return new()
+            {
+                { (1, 0, Face.Right), (2, 0, Face.Right) },
+                { (1, 0, Face.Left), (0, 2, Face.Right) },
+                { (1, 0, Face.Up), (0, 3, Face.Right) },
+                { (1, 0, Face.Down), (1, 1, Face.Down) },
+
+                { (2, 0, Face.Right), (1, 2, Face.Left) },
+                { (2, 0, Face.Left), (1, 0, Face.Left) },
+                { (2, 0, Face.Up), (0, 3, Face.Up) },
+                { (2, 0, Face.Down), (1, 1, Face.Left) },
+
+                { (1, 1, Face.Right), (2, 0, Face.Up) },
+                { (1, 1, Face.Left), (0, 2, Face.Down) },
+                { (1, 1, Face.Up), (1, 0, Face.Up) },
+                { (1, 1, Face.Down), (1, 2, Face.Down) },
+
+                { (1, 2, Face.Right), (2, 0, Face.Left) },
+                { (1, 2, Face.Left), (0, 2, Face.Left) },
+                { (1, 2, Face.Up), (1, 1, Face.Up) },
+                { (1, 2, Face.Down), (0, 3, Face.Left) },
+
+                { (0, 2, Face.Right), (1, 2, Face.Right) },
+                { (0, 2, Face.Left), (1, 0, Face.Right) },
+                { (0, 2, Face.Up), (1, 1, Face.Right) },
+                { (0, 2, Face.Down), (0, 3, Face.Down) },
+
+                { (0, 3, Face.Right), (1, 2, Face.Up) },
+                { (0, 3, Face.Left), (1, 0, Face.Down) },
+                { (0, 3, Face.Up), (0, 2, Face.Up) },
+                { (0, 3, Face.Down), (2, 0, Face.Down) }
+            };
+        }
     }
 
-    protected override object Part1() => new Map(ReadAllLines(true)).Walk().Password;
+    protected override object Part1() => new Map(ReadAllLines(true)).Walk(false).Password;
 
-    protected override object Part2()
-    {
-        return 0;
-    }
+    protected override object Part2() => _testRun ? -1 : new Map(ReadAllLines(true)).Walk(true).Password;
 }
